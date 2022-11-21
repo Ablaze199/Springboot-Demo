55403ed5ca8c72fd2454c5c5dcf87ec2246a5087
Ablaze99 <femiemma99@gmail.com>
2022-11-21 21:10:44 +0100

    employee

--- src/main/java/com/spring/demo/service/serviceImplementaion/EmployeeServiceImpl.java
+++ src/main/java/com/spring/demo/service/serviceImplementaion/EmployeeServiceImpl.java
@@ -7,6 +7,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.Optional;
 
 @Service
 
@@ -18,9 +19,24 @@ public class EmployeeServiceImpl implements EmployeeService {
     public List<Employee> getAllEmployees() {
         return employeeRepository.findAll();
     }
+
     @Override
-    public void saveEmployee (Employee employee){
+    public void saveEmployee(Employee employee) {
         this.employeeRepository.save(employee);
 
     }
+
+    @Override
+    public Employee getEmployeeById(long id) {
+        Optional<Employee> optional = employeeRepository.findById(id);
+        Employee employee = null;
+        if (optional.isPresent()) {
+            employee = optional.get();
+        } else {
+            throw new RuntimeException(" Employee not found for id :: " + id);
+        }
+        return employee;
+    }
 }
+
+
--- src/main/java/com/spring/demo/service/springcontroler/EmployeeController.java
+++ src/main/java/com/spring/demo/service/springcontroler/EmployeeController.java
@@ -1,4 +1,4 @@
-package com.spring.demo.springcontroler;
+package com.spring.demo.service.springcontroler;
 
 import com.spring.demo.model.Employee;
 import com.spring.demo.service.EmployeeService;
@@ -6,6 +6,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 
 @Controller
 public class EmployeeController {
@@ -18,12 +20,18 @@ public class EmployeeController {
         model.addAttribute("listEmployees", employeeService.getAllEmployees());
         return "index";
     }
-    @GetMapping("/showNewEmployeeform")
-    public String showNewEmployeeform( Model model){
-        //create model attribute to bind data
+    @GetMapping("/showNewEmployeeForm")
+    public String showNewEmployeeForm(Model model) {
+        // create model attribute to bind form data
         Employee employee = new Employee();
         model.addAttribute("employee", employee);
         return "new_employee";
+    }
 
+    @PostMapping("/saveEmployee")
+    public String saveEmployee(@ModelAttribute("employee") Employee employee) {
+        // save employee to database
+        employeeService.saveEmployee(employee);
+        return "redirect:/";
     }
 }
